d74494e6e30e9b929ca3166d7045d27d1e52e58c
Eduardo Soto <jesaeduardo1999@gmail.com>
2021-10-02 07:44:57 -0600

    comeen

---
+++ CRM_Analisis_WEB/Controllers/BitacoraFlujosController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoGraduacion_WEB.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoGraduacion_WEB.Controllers
+{
+    public class BitacoraFlujosController : Controller
+    {
+        private readonly DataContext _dataContext;
+
+        public BitacoraFlujosController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public IActionResult BitacoraFlujos()
+        {
+            return View();
+        }
+
+    }
+}
